6e99fca96ea618ea3ef88010f5da49df21d54b95
zhangyi-zjuer <zhangyi.zjuer@gmail.com>
2014-06-13 11:25:06 +0800

    bug fix

--- app/user/views.py
+++ app/user/views.py
@@ -14,7 +14,7 @@ mod = Blueprint('user', __name__, template_folder='templates', static_folder='st
 @mod.route('/login', methods=['GET', 'POST'])
 def login():
     if g.user is not None and g.user.is_authenticated():
-        return redirect(request.args.get("next") or request.referrer or url_for('index'))
+        return redirect(redirect_url())
 
     form = g.form
     if form.validate_on_submit():
@@ -28,7 +28,7 @@ def login():
 
         if not user:
             flash(u"Invalid User !")
-            return redirect(request.args.get("next") or request.referrer or url_for('index'))
+            return redirect(redirect_url())
 
         if hashlib.md5(password.encode('utf-8')).hexdigest() == user.password:
             remember_me = True
@@ -37,22 +37,26 @@ def login():
                 session.pop('remember_me', None)
 
             login_user(user, remember=remember_me)
-            return redirect(request.args.get("next") or request.referrer or url_for('index'))
+            return redirect(redirect_url())
 
         flash(u"Wrong username or password !")
-        return redirect(request.args.get("next") or request.referrer or url_for('index'))
+        return redirect(redirect_url())
 
     flash(u"Please Enter your Username and Password !")
-    return redirect(request.args.get("next") or request.referrer or url_for('index'))
+    return redirect(redirect_url())
 
 
 @mod.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(request.args.get("next") or request.referrer or url_for('index'))
+    return redirect(redirect_url())
 
 
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(id)
+
+
+def redirect_url():
+    return request.args.get("next") or request.referrer or url_for('index')
